9b058e3f4dee6ac3a02cd5189da15eb738ac01b0
flash1zx <167422133+flash1zx@users.noreply.github.com>
2024-05-17 18:06:52 +0545

    Add files via upload

---
+++ Project1.py
@@ -0,0 +1,51 @@
+print("Welcome to my quiz game!")
+
+playing = input("Do you want to play? ")
+
+if playing .lower() == "yes":   
+    print("Ok! let's play :)")
+    score = 0
+    
+else:
+    quit()
+
+answer = input("What does CPU stand for? ")
+if answer.lower() == "central processing unit":
+    print("You're correct!")
+    score += 1
+    
+else:
+    print("You're incorrect, Try again!!")
+    quit()
+    
+    
+answer = input("What does GPU stand for? ")
+if answer.lower() == "graphics processing unit":
+    print("You're correct!")
+    score += 1
+    
+else:
+    print("You're incorrect, Try again!!")
+    quit()
+    
+
+answer = input("What does RAM stand for? ")
+if answer.lower() == "random access memory":
+    print("You're correct!")
+    score += 1
+    
+else:
+    print("You're incorrect, Try again!!")
+    quit()
+    
+    
+answer = input("What does PSU stand for? ")
+if answer.lower() == "power supply unit":
+    print("You're correct!")
+    score += 1
+    
+else:
+    print("You're incorrect, Try again!!")
+    quit()
+    
+print("You got " + str(score) + " questions correct!")
